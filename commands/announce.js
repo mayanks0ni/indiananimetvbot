f5a659e3a625b77fe64fdfb02810ddeaf04bec27
mayanks0ni <56229897+mayanks0ni@users.noreply.github.com>
2020-03-11 17:31:15 +0530

    Update announce.js

--- commands/announce.js
+++ commands/announce.js
@@ -2,12 +2,12 @@ const Discord = module.require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 	if (!message.member.permissions.has("MANAGE_MESSAGES")) return message.channel.send(new Discord.MessageEmbed().setTitle('You Don\'t Have Enough Permissions!').setColor(0xff0000).setFooter("IAT Bot").setTimestamp());
-	let ancmessage;
+	let ancmessage = args.slice(2).join(" ")
+	let ancmessage1 = args.slice(1).join(" ");
 	let mchannel = message.mentions.channels.first()
 	if(!ancmessage)return message.channel.send(new Discord.MessageEmbed().setTitle('This Command Is Used Like This \`+announce [channel] [message]\` Or \`+announce [message]\` To Announce In The Channel Used!').setColor(0xff0000).setFooter("IAT Bot").setTimestamp());
 	message.delete()
 	if (mchannel) {
-		ancmessage = args.slice(2).join(" ");
 		const anc = new Discord.MessageEmbed()
 			.setDescription(`${ancmessage}`)
 			.setFooter('IAT Bot')
@@ -15,9 +15,9 @@ module.exports.run = async (bot, message, args) => {
 			.setColor(0x6bffe1)
 		mchannel.send(anc);
 	} else {
-		ancmessage = args.slice(1).join(" ");
+		
 		const anc1 = new Discord.MessageEmbed()
-			.setDescription(`${ancmessage}`)
+			.setDescription(`${ancmessage1}`)
 			.setFooter('IAT Bot')
 			.setTimestamp()
 			.setColor(0xffc859)
