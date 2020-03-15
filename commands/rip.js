18321d57f4e023578fbf806c10565fd1009efe3b
mayanks0ni <56229897+mayanks0ni@users.noreply.github.com>
2020-03-15 19:28:35 +0530

    Update rip.js

--- commands/rip.js
+++ commands/rip.js
@@ -5,9 +5,8 @@ module.exports.run = async (bot, message, args) => {
     const canvas = Canvas.createCanvas(720, 1280);
     const ctx = canvas.getContext("2d");
   const user = message.mentions.users.first();
+if(!user) return message.channel.send(new Discord.MessageEmbed().setAuthor(`${message.author.tag}`).setTitle("This Command Is Used \`+rip [user]\`!").setColor(0xff0000).setFooter("IAT Bot").setTimestamp());
   const user2 = user.displayAvatarURL({format: 'png', dynamic: true, size: 1024})
-
-  if(!user) return message.channel.send(new Discord.MessageEmbed().setAuthor(`${message.author.tag}`).setTitle("This Command Is Used \`+rip [user]\`!").setColor(0xff0000).setFooter("IAT Bot").setTimestamp());
 
    const background = await Canvas.loadImage(`https://cdn.discordapp.com/attachments/564520348821749766/687972262510329856/rest-in-peace-rip-headstone-blank-template-imgflip-53245711.png`)
    
