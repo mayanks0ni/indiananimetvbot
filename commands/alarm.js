12729766265b76b1123e634eeccbd70968d0d756
mayanks0ni <56229897+mayanks0ni@users.noreply.github.com>
2020-03-22 18:31:18 +0530

    Add files via upload

---
+++ commands/alarm.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+const ms = require("ms");
+
+module.exports.run = async(bot, message, args) => {
+   const time = args[1];
+   const reminder = args.slice(2).join(" ");
+   if(!time || !reminder) return message.channel.send(new Discord.MessageEmbed().setAuthor(`${message.author.tag}`).setTitle("This Command Is Used Like This \`+alarm [duration (1m/1h/1d)] [reminder]\`").setFooter("IAT Bot").setColor(0xff0000).setTimestamp());
+   message.channel.send(new Discord.MessageEmbed().setAuthor(`${message.author.tag}`).setTitle("⏰ Your Alarm Has Been Set To Remind You! ⏰").setFooter("IAT Bot").setColor("RANDOM").setTimestamp());
+   const alarmEmbed = new Discord.MessageEmbed()
+   .setAuthor(`${message.author.tag}`)
+   .setTitle("⏰ Alarm ⏰")
+   .setDescription(`**Your Reminder To Do ${reminder}**!`)
+   .setColor("RANDOM")
+   .setFooter("IAT Bot")
+   .setTimestamp()
+
+   setTimeout( function(){
+   	message.author.send(alarmEmbed);
+   }, ms(time));
+}
+
+module.exports.config = {
+	name: "alarm",
+	description: "A Command To Set A Reminder Which Will Alarm You By DMing You!",
+	usage: "+alarm [duration (1m/1h/1d)] [reminder]",
+	accessableby: "Members",
+	aliases: ["reminder"]
+}
---
+++ commands/reminder.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js");
+const ms = require("ms");
+
+module.exports.run = async(bot, message, args) => { 
+   
+   const time = "3h";
+   const reminder = args.slice(2).join(" ");
+   message.channel.send(new Discord.MessageEmbed().setAuthor(`${message.author.tag}`).setTitle("Safety Reminder!").setDescription("**Safety Reminder Has Been Set! It will DM You Every 3 hrs To Remind You To Wash/Sanitize Your Hands With Some Extra Tips!**").setFooter("IAT Bot").setColor("RANDOM").setTimestamp());
+   const alarmEmbed = new Discord.MessageEmbed()
+   .setAuthor(`${message.author.tag}`)
+   .setTitle("⏰ Reminder ⏰")
+   .setDescription(`**Wash your hands often with soap and water for at least 20 seconds especially after you have been in a public place, or after blowing your nose, coughing, or sneezing.\nIf soap and water are not readily available, use a hand sanitizer that contains at least 60% alcohol.\n Cover all surfaces of your hands and rub them together until they feel dry. \nAvoid touching your eyes, nose, and mouth with unwashed hands.**!`)
+   .setColor("RANDOM")
+   .setFooter("IAT Bot")
+   .setTimestamp()
+
+   setInterval( function(){
+   	message.author.send(alarmEmbed);
+   }, ms(time));
+
+}
+
+module.exports.config = {
+	name: "remind",
+	description: "A Command To Set A Reminder To Protect You From Corona Virus!",
+	usage: "+remind",
+	accessableby: "Members",
+	aliases: [""]
+}
