7840a90cee56f54aab4cb1eb3241708f8a666f2e
mayanks0ni <56229897+mayanks0ni@users.noreply.github.com>
2020-03-30 13:51:29 +0530

    Update covid.js

--- commands/covid.js
+++ commands/covid.js
@@ -3,9 +3,9 @@ const Discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
   let countryName = args.slice(1).join(" ");
-  let res = await covid.getCountry({country: countryName}).catch(err =>{
-   return message.channel.send(new Discord.MessageEmbed().setTitle("Error!").setDescription(err).setFooter("IAT Bot").setTimestamp().setColor(0xff0000))
-  });
+  if(!countryName) return message.channel.send(new Discord.MessageEmbed().setTitle("Error!").setDescription("**Please Specify The Country\'s Name!**").setFooter("IAT Bot").setTimestamp().setColor(0xff0000))
+  let res = await covid.getCountry({country: countryName})
+  if(`${res.country}` === "undefined") return message.channel.send(new Discord.MessageEmbed().setTitle("Error!").setDescription("**Invalid Country\'s Name!**").setFooter("IAT Bot").setTimestamp().setColor(0xff0000))
   message.channel.send(new Discord.MessageEmbed().setTitle(`Live Status Of ${res.country}!`).addField("**Total Cases**", `${res.cases}`).addField("**Today Cases**", `${res.todayCases}`).addField("**Total Deaths**", `${res.deaths}`).addField("**Today Deaths**", `${res.todayDeaths}`).addField("**Recovered**", `${res.recovered}`).addField("**Active**", `${res.active}`).addField("**Critical**", `${res.critical}`).setFooter("IAT Bot").setTimestamp().setColor("RANDOM"))
 }
 
