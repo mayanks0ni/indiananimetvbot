11300e012d9d5a6a5f40e89d6a379b8097631d2a
mayanks0ni <56229897+mayanks0ni@users.noreply.github.com>
2020-04-15 18:00:32 +0530

    Update google.js

--- commands/google.js
+++ commands/google.js
@@ -1,23 +1,33 @@
 const Discord = require("discord.js");
+const app = require("node-server-screenshot");
 
 module.exports.run = async (bot, message, args) => {
-  const query = args.slice(1).join(" ");
-  if(!query) return message.channel.send(new Discord.MessageEmbed().setTitle("This Command Is Used Like This \`+google [query]\`!").setColor(0xff0000).setFooter("IAT Bot").setTimestamp());
-  const googleEmbed = new Discord.MessageEmbed()
-  .setAuthor("Google Search Results!")
-  .setTitle("Click Here To View Results.")
-  .setURL(`https://lmgtfy.com/?q=${query}`)
-  .setThumbnail(`https://cdn.discordapp.com/attachments/564520348821749766/695586317068664891/1585911065485757156151.jpg`)
-  .setFooter("IAT Bot")
-  .setTimestamp()
-  .setColor("RANDOM")
-  message.channel.send(googleEmbed);
+  const query = args.slice(2).join(" ");
+  const type = args[1];
+  const searchType = ["search"];
+  if(!query || !type || !type.includes(searchType)) return message.channel.send(new Discord.MessageEmbed().setTitle("This Command Is Used Like This \`+google [search] [query]\`!").setColor(0xff0000).setFooter("IAT Bot").setTimestamp());
+ if(type === "search"){
+   app.fromURL(`https://www.google.com/search?q=${query}`, "./googleresult.png", function(err){
+     if(err){
+       return message.channel.send(new Discord.MessageEmbed().setTitle(`Error: \`${err}\``).setColor(0xff0000).setFooter("IAT Bot").setTimestamp());
+     }else{
+       const googleEmbed = new Discord.MessageEmbed()
+       .setAuthor(`${message.author.tag}`)
+       .setTitle("Search Results")
+       .attachFiles({ attachment: './googleresult.png' })
+       .setImage('attachment://googleresult.png')
+       .setFooter("IAT Bot")
+       .setTimestamp()
+       .setColor("RANDOM")
+      message.channel.send(googleEmbed)
+       }});
+ }
 }
 
 module.exports.config = {
   name: "google",
   description: "A Command To Google The Query!",
-  usage: "+google [query]",
+  usage: "+google [search] [query]",
   accessableby: "Members",
   aliases: [""]
 }
